0c0158f99fa8a0ee2b410fed577574e0aec2373a
tmucha82 <tmucha82@gmail.com>
2017-04-15 15:31:17 +0200

    CyclicRotation

---
+++ src/test/java/com/infotarget/codility/java/CyclicRotationTest.java
@@ -0,0 +1,20 @@
+package com.infotarget.codility.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CyclicRotationTest {
+
+    @Test
+    public void testSolution1() throws Exception {
+        //TODO
+
+    }
+
+    @Test
+    public void testSolution2() throws Exception {
+        //TODO
+
+    }
+}
---
+++ src/test/scala/com/infotarget/codility/scala/CyclicRotationTest.scala
@@ -0,0 +1,15 @@
+package com.infotarget.codility.scala
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by Tomek on 2017-04-15.
+  */
+class CyclicRotationTest extends FunSuite {
+
+  test("testSolution") {
+    //TODO
+
+  }
+
+}
