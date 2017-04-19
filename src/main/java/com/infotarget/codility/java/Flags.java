4d80d9f0d8f1c0d7ce6ebe449df5fa90304dfd79
tmucha <tmucha@cyfrowypolsat.pl>
2017-04-19 21:45:32 +0200

    Flags - final

--- src/main/java/com/infotarget/codility/java/Flags.java
+++ src/main/java/com/infotarget/codility/java/Flags.java
@@ -1,15 +1,12 @@
 package com.infotarget.codility.java;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * A non-empty zero-indexed array A consisting of N integers is given.
- * <p>
+ * <p/>
  * A peak is an array element which is larger than its neighbours. More precisely, it is an index P such that 0 < P < N − 1 and A[P − 1] < A[P] > A[P + 1].
- * <p>
+ * <p/>
  * For example, the following array A:
- * <p>
+ * <p/>
  * A[0] = 1
  * A[1] = 5
  * A[2] = 3
@@ -23,27 +20,27 @@ import java.util.List;
  * A[10] = 6
  * A[11] = 2
  * has exactly four peaks: elements 1, 3, 5 and 10.
- * <p>
+ * <p/>
  * You are going on a trip to a range of mountains whose relative heights are represented by array A, as shown in a figure below. You have to choose how many flags you should take with you. The goal is to set the maximum number of flags on the peaks, according to certain rules.
- * <p>
- * <p>
- * <p>
+ * <p/>
+ * <p/>
+ * <p/>
  * Flags can only be set on peaks. What's more, if you take K flags, then the distance between any two flags should be greater than or equal to K. The distance between indices P and Q is the absolute value |P − Q|.
- * <p>
+ * <p/>
  * For example, given the mountain range represented by array A, above, with N = 12, if you take:
- * <p>
+ * <p/>
  * two flags, you can set them on peaks 1 and 5;
  * three flags, you can set them on peaks 1, 5 and 10;
  * four flags, you can set only three flags, on peaks 1, 5 and 10.
  * You can therefore set a maximum of three flags in this case.
- * <p>
+ * <p/>
  * Write a function:
- * <p>
+ * <p/>
  * class Solution { public int solution(int[] A); }
  * that, given a non-empty zero-indexed array A of N integers, returns the maximum number of flags that can be set on the peaks of the array.
- * <p>
+ * <p/>
  * For example, the following array A:
- * <p>
+ * <p/>
  * A[0] = 1
  * A[1] = 5
  * A[2] = 3
@@ -57,52 +54,69 @@ import java.util.List;
  * A[10] = 6
  * A[11] = 2
  * the function should return 3, as explained above.
- * <p>
+ * <p/>
  * Assume that:
- * <p>
+ * <p/>
  * N is an integer within the range [1..400,000];
  * each element of array A is an integer within the range [0..1,000,000,000].
  * Complexity:
- * <p>
+ * <p/>
  * expected worst-case time complexity is O(N);
  * expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
  * Elements of input arrays can be modified.
  */
 public class Flags {
+
     public int solution(int[] A) {
-        Integer[] peaks = findPeaks(A);
+        boolean[] peaks = findPeaks(A);
 
         if (peaks.length < 2) {
             return peaks.length;
         }
 
-        int maxDistance = peaks[peaks.length - 1] - peaks[0];
-        int maxF = (int) (Math.sqrt(maxDistance) + 1);
 
-        int max = 0;
-        for (int k = 2; k <= maxF; k++) {
-            int lastPick = peaks[0];
-            int used = 1;
-            for (int c = 1; c < peaks.length && used < k; c++) {
-                if (peaks[c] - lastPick >= k) {
-                    lastPick = peaks[c];
-                    used++;
-                }
+        int min = 2;
+        int max = (int) Math.floor(Math.sqrt(A.length));
+        int result = 0;
+
+        while (min <= max) {
+            int K = (max + min) / 2;
+            if (!check(A, K, peaks)) {
+                //we failed to put K flags, then try to check a smaller value.
+                max = K - 1;
+            } else {
+                //we succeed to put K flags, update the max_flags and try a larger value.
+                min = K + 1;
+                result = K;
             }
-            max = Math.max(used, max);
         }
-
-        return max;
+        return result;
     }
 
-    Integer[] findPeaks(int[] A) {
-        List<Integer> peaks = new ArrayList<>();
+    boolean[] findPeaks(int[] A) {
+        boolean[] peaks = new boolean[A.length];
         for (int i = 1; i < A.length - 1; i++) {
             if (A[i - 1] < A[i] && A[i] > A[i + 1]) {
-                peaks.add(i);
+                peaks[i] = true;
             }
         }
-        return peaks.toArray(new Integer[peaks.size()]);
+        return peaks;
     }
 
+    boolean check(int[] A, int K, boolean[] peaks) {
+        int pos = 0;
+        int flags = K;
+        while (pos < A.length && K > 0) {
+            if (peaks[pos]) {
+                flags--;
+                if (flags == 0) {
+                    return true;
+                }
+                pos += K;
+            } else {
+                pos++;
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/com/infotarget/codility/java/FlagsTest.java
+++ src/test/java/com/infotarget/codility/java/FlagsTest.java
@@ -1,14 +1,34 @@
 package com.infotarget.codility.java;
 
 import org.junit.Assert;
-import org.junit.Ignore;
+import org.junit.Test;
 
 public class FlagsTest {
 
     private Flags flags = new Flags();
 
-    @Ignore
+    @Test
     public void testSolution() throws Exception {
         Assert.assertEquals(3, flags.solution(new int[]{1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2}));
+    }
+
+    @Test
+    public void testFindPeaks() {
+        Assert.assertArrayEquals(
+                new boolean[]{false, true, false, true, false, true, false, false, false, false, true, false},
+                flags.findPeaks(new int[]{1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2}));
+    }
+
+    @Test
+    public void testCheck() {
+        /**
+         * two flags, you can set them on peaks 1 and 5;
+         * three flags, you can set them on peaks 1, 5 and 10;
+         * four flags, you can set only three flags, on peaks 1, 5 and 10.
+         */
+        int[] area = {1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2};
+        Assert.assertTrue(flags.check(area, 2, flags.findPeaks(area)));
+        Assert.assertTrue(flags.check(area, 3, flags.findPeaks(area)));
+        Assert.assertFalse(flags.check(area, 4, flags.findPeaks(area)));
     }
 }
